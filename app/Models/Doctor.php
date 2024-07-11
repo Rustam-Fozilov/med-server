c5aedb50bc6a7e0db43be62a40121f930b368727
rustamdotdev <thisisrustam@gmail.com>
2024-07-11 19:42:29 +0300

    ready: models

---
+++ app/Models/Doctor.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Doctor extends Model
+{
+    use HasFactory;
+
+    protected $table = 'doctors';
+
+    protected $fillable = [
+        'user_id',
+        'birth_year',
+        'experience',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\DoctorController;
+use App\Http\Controllers\ServiceController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::resources([
+    'doctors' => DoctorController::class,
+    'services' => ServiceController::class,
+]);
